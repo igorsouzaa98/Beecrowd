ee68367f3bc014d26af50c00897840caf7b06c1f
Igor Souza <igorsouzaatb@hotmail.com>
2022-07-04 12:13:59 -0300

    adicionando arquivos da atividade Beecrowd

--- 1079.js
+++ 1079.js
@@ -1,23 +1,10 @@
 var input = require('fs').readFileSync('stdin','utf8')
 var lines = input.split('\n');
 
-var l1 = lines[1].split(' ')
-var l2 = lines[2].split(' ')
-var l3 = lines[3].split(' ')
+var n = parseInt(lines[0])
 
-var array1 = l1.map(x=>parseFloat(x))
-var [a, b, c] = array1
-
-var array2 = l2.map(x=>parseFloat(x))
-var[e, f, g] =  array2
-
-var array3 = l3.map(x=>parseFloat(x))
-var[h, i, j]= array3
-
-var media1 = (((2*a)+(3*b)+(5*c))/(2 + 3 + 5)).toFixed(1)
-var media2 = (((2*e)+(3*f)+(5*g))/(2 + 3 + 5)).toFixed(1)
-var media3 = (((2*h)+(3*i)+(5*j))/(2 + 3 + 5)).toFixed(1)
-
-console.log(media1)
-console.log(media2)
-console.log(media3)
+for (i = 1; i <= n; i++){
+    var [v1,v2, v3] = lines[i].split(' ').map(x=>parseFloat(x))
+    var media = ((v1 * 2)+(v2 * 3)+(v3 * 5))/10
+    console.log(media.toFixed(1))
+}
--- 1095.js
+++ 1095.js
@@ -1,10 +1,8 @@
 var input = require('fs').readFileSync('stdin','utf8')
 var lines = input.split('\n');
 
-var j = 60
+var i, j
 
-
-for(i=j;i<60;i--){
-    var resultado1 = j - 5
-    console.log(resultado1)
+for(i = 1 , j = 60; j> -1; i+=3,j-=5 ){
+    console.log(`I = ${i} J=${j}`)
 }
